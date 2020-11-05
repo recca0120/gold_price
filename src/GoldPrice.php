fdafcb052aedb2efb7971b5fdf33f2c08a5619a1
recca0120 <recca0120@gmail.com>
2020-11-05 16:35:11 +0800

    finish

--- src/GoldPrice.php
+++ src/GoldPrice.php
@@ -16,6 +16,32 @@ class GoldPrice
     {
         $html = $this->fetcher->fetch();
 
-        preg_match_all('//', $html, $matches);
+        $rows = array_map(function ($row) {
+            return $this->parseCols($row);
+        }, $this->parseRows($html));
+
+        array_shift($rows);
+
+        return $rows;
+    }
+
+    /**
+     * @param $html
+     * @return mixed
+     */
+    private function parseRows($html)
+    {
+        preg_match_all('/<tr>(?<rows>(.*?))<\/tr>/s', $html, $matches);
+
+        return $matches['rows'];
+    }
+
+    private function parseCols($row)
+    {
+        preg_match_all('/<t(h|d)[^>]*>(?<cols>(.*?))<\/t(h|d)>/s', $row, $matches);
+
+        return array_map(function ($col) {
+            return strip_tags($col);
+        }, $matches['cols']);
     }
 }
